dc8811d22b614b256b68e501521c44cd9bf377fc
J. R. Schmid <jrs+git@weitnahbei.de>
2014-07-08 01:31:10 +0200

    Does everything it was intended to.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,36 @@
 class PagesController < ApplicationController
-  before_filter :set_book
+  before_filter :set_book, :initialize_session
 
   def show
-    @page = params[:page_id] ? Page.find(@book, params[:page_id]) : Page.first_with_content(@book)
+    @page = Page.find(@book, params[:page_id] || 1)
+  rescue
+    @book = Book.first                     # To guard against nonexisting books.
+    @page = Page.first_with_content(@book) # To guard against nonexisting pages.
   end
   
   def find
-    @search_string = params[:search_string]
+    @search_string = params[:search_string] || params[:page][:search_string] # Dirty, I know.
     begin
       @page = Page.find_by_search_string(@book, @search_string)
+      session[:previous_searches].shift if session[:previous_searches].length > 50
+      session[:previous_searches] << @search_string
+      session[:previous_searches].uniq!
     rescue RuntimeError
-      @page = Page.first_with_content(@book)
-      flash.now[:alert] = "Keine Suchergebnisse"
+      flash.now[:alert] = "Keine Suchergebnisse. Versuchen Sie die Eingabe einer Wurzel anstelle eines Wortes."
     end
-    render :show
+    respond_to do |format|
+      format.html { @page ||= Page.first_with_content(@book); render :show }
+      format.json { render json: {page: @page, previous_searches: session[:previous_searches].reverse} }
+    end
   end
   
   private
   
   def set_book
     @book = Book.find_by_name(params[:book_name] || Book.first.name)
+  end
+
+  def initialize_session
+    @previous_searches = session[:previous_searches] ||= []
   end
 end
