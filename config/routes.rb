b3714ab12b1aef4374ba93447c90572aa02669b3
Rainer Defender <materialdefender2306@gmail.com>
2014-07-08 18:23:14 +0200

    Non-JS parts working fine again.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages#show'
-  get '/:book_name/:page_id', to: 'pages#show'
-  get '/:search_string', to: 'pages#find'
-  post '/pages', to: 'pages#find'
+  root                 to: 'pages#show'
+  get  '/:book/:page', to: 'pages#show'
+  get  '/:search',     to: 'pages#find'
+  post '/pages',       to: 'pages#find'
 end
