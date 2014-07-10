907b5de16b6a678b6f9b83a21a4b4af907cac89d
Rainer Defender <materialdefender2306@gmail.com>
2014-07-10 14:27:03 +0200

    More bugfixes. Enabling TurboLinks again. Making production mode work.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= # require turbolinks
+//= require turbolinks
 //= require bootstrap
 //= require jquery_mb_extruder.all
 //= require jquery.cookie
