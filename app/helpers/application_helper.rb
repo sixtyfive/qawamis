cd999d26fb475e1a4d304b5b088704beea0de747
Rainer Defender <materialdefender2306@gmail.com>
2014-07-10 19:33:58 +0200

    Make the search history a little less confusing.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def current_search
-    params[:search] || JSON.parse(cookies[:search_history] || '[]').last || String.new
+    last_search
+  end
+
+  def last_search
+    JSON.parse(cookies[:search_history]).last unless cookies[:search_history].blank?
   end
 end
