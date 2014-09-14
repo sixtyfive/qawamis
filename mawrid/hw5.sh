2da17d32d2f6c2cdeb2f229aa4b57e1f0a6a0f1d
J. R. Schmid <jrs+git@weitnahbei.de>
2014-09-14 12:51:54 +0200

    Whoopsiedaisies!

--- mawrid/hw5.sh
+++ mawrid/hw5.sh
@@ -3,7 +3,6 @@
 cat hw5.tpl.txt > mr-aa-hw5-index.js
 i=26
 while read line; do
-  hw5[26]='ﺎﺑﺩ';
   echo -e "/* $((i - 25)) */ hw5[$i]='$line';" >> mr-aa-hw5-index.js
   i=$((i + 1))
 done < hw5.txt
