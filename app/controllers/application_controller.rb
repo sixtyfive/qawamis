4393ebf25e53e0169ddcb3017fea94b6a1406494
J. R. Schmid <jrs+git@weitnahbei.de>
2015-09-29 15:36:35 +0200

    Lalalala.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :set_locale
+  before_filter :set_cache_buster
+
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -9,5 +11,12 @@ class ApplicationController < ActionController::Base
   
   def set_locale
     I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
+  end
+
+  # http://stackoverflow.com/questions/711418/how-to-prevent-browser-page-caching-in-rails
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "#{1.year.ago}"
   end
 end
