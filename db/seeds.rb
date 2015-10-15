d7bd4bbcbb2cf84332a291c2d9173be1adeb6af3
J. R. Schmid <materialdefender2306@gmail.com>
2015-10-15 17:12:29 +0200

    Neues Buch (al-Munjid) hinzugefügt.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,27 +4,29 @@
 require 'v8'
 js = String.new
 roots = Hash.new
-%w{mr-aa-hw5-index.js mr-aa-indexes.js mr-aa-hw3-index.js mr-mr-indexes.js}.each do |file|
+%w{mr-aa-hw5-index.js mr-aa-indexes.js mr-aa-hw3-index.js mr-mr-indexes.js mr-aa-amj-index.js}.each do |file|
   js += IO.read(File.join(Rails.root, 'mawrid', file))
 end
 cxt = V8::Context.new
 cxt.eval(js)
+
                                 # Count from cover,      # Mawrid URL parameter,
 books = [                       # starting with "1"      # add "1", but check!
   {name: 'hw5', language: 'de', first_numbered_page: 26, number_of_pages: 1478},
   {name: 'hw4', language: 'en', first_numbered_page: 14, number_of_pages: 1316},
-  {name: 'hw3', language: 'en', first_numbered_page: 19, number_of_pages: 1130},
   {name: 'll',  language: 'en', first_numbered_page: 39, number_of_pages: 3079},
   {name: 'ls',  language: 'en', first_numbered_page:  3, number_of_pages:   85},
   {name: 'sg',  language: 'en', first_numbered_page: 20, number_of_pages: 1263},
   {name: 'ha',  language: 'en', first_numbered_page: 21, number_of_pages:  919},
   {name: 'la',  language: 'ar', first_numbered_page:  1, number_of_pages: 4979},
+  {name: 'maw', language: 'en', first_numbered_page: 16, number_of_pages: 1256},
+  {name: 'amj', language: 'ar', first_numbered_page: 22, number_of_pages: 1036},
+  {name: 'hw3', language: 'en', first_numbered_page: 19, number_of_pages: 1130},
   {name: 'br',  language: 'en', first_numbered_page: 26, number_of_pages: 1092},
   {name: 'pr',  language: 'en', first_numbered_page:  9, number_of_pages:  177},
   {name: 'vq',  language: 'en', first_numbered_page: 24, number_of_pages:  757},
   {name: 'mgf', language: 'en', first_numbered_page: 35, number_of_pages:  880},
-  {name: 'vi',  language: 'en', first_numbered_page: 51, number_of_pages:  400},
-  {name: 'maw', language: 'en', first_numbered_page: 16, number_of_pages: 1256}
+  {name: 'vi',  language: 'en', first_numbered_page: 51, number_of_pages:  400}
   # After adding the book here, move the cover page
   # image to page_0000.png and move a blank page to
   # page_0001.png instead.
@@ -40,15 +42,18 @@ books = [                       # starting with "1"      # add "1", but check!
   # requests! :-)
 ].each do |book|
   _book = Book.create(name: book[:name], language: book[:language], first_numbered_page: book[:first_numbered_page])
+  # _book = Book.new(name: book[:name], language: book[:language], first_numbered_page: book[:first_numbered_page])
   roots[_book.name.to_sym] = cxt[_book.name.to_sym]
   ActiveRecord::Base.transaction do
     (1..book[:number_of_pages]).each do |page|
-      puts "#{book[:name]}: [##{page}] (#{page-_book.first_numbered_page} - #{roots[_book.name.to_sym][page-1]})"
+      name = book[:name]
+      root = roots[_book.name.to_sym][page-1]
+      root = root.gsub(',', '') if root
+      puts "#{name}: [##{page}] (#{page-_book.first_numbered_page} - #{root})"
       _book.pages << Page.create(
         number: page - _book.first_numbered_page,
         last_root: roots[_book.name.to_sym][page-1]
       )
     end
   end
-
 end
