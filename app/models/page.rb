1473491eebfaa2c4db4b6125375b5ce7a1afcf6b
J. R. Schmid <jrs+git@weitnahbei.de>
2015-12-10 00:44:31 +0100

    Neuer Suchalgorithmus auf @jonas' Vorschlag hin implementiert. Automatisierte Tests für Suchergebnisse implementiert. Liste von Suchen erstellt, die schon einmal fehlerhaft waren. Korrekturen an der Wurzelliste des HW5 eingearbeitet. Lokale Datenbank neu befüllt. Anpassungen in Vorbereitung für Rails 5 vorgenommen.

--- app/models/page.rb
+++ app/models/page.rb
@@ -23,24 +23,20 @@ class Page < ActiveRecord::Base
     #
     # Easiest and fastest: the root is already in the
     # list of roots for the current scope of book/page.
-    logger.debug("*** find_by_root (1): searching for #{search.inspect}")
+    logger.debug("*** find_by_root: searching for #{search.inspect}")
     unless page = self.find_by_last_root(search)
       #
       # No luck so far. To make things easier, substitute
       # hamzas for alifs and alif maksuras for yas (originally
       # do_search().
       search = search.gsub(/[إآٱأءﺀﺀﺁﺃﺅﺇﺉ]/, 'ا').gsub(/ﻯ/,'ﻱ')
-      logger.debug("*** find_by_root (2): searching for #{search.inspect}")
       unless page = self.find_by_last_root(search)
         #
         # Still no luck. Time to modify the search string even
         # further. make_suggestions() seems to try and take a
         # guess at what other roots could be in the vincinity
         # of the searched-for root.
-        if search = most_likely_closeby_root(search)
-          logger.debug("*** find_by_root (3): searching for #{search.inspect}")
-          page = self.find_by_last_root(search)
-        else
+        unless page = self.most_likely_page(search)
           logger.debug("*** find_by_root: all search attempts turned out empty.")
           page = nil
         end
@@ -77,51 +73,17 @@ class Page < ActiveRecord::Base
 
   private
 
-  # Basically binarySearch() as found in Abdurahmans code.
-  # He says its copyright holder to be Nicholas C. Zakas
-  # who seems to have released his implementation under a
-  # MIT license. I hope that's true. A big thank you to
-  # both, Nicholas and Abdurahman!
-  def self.most_likely_closeby_root(value)
-    # Same as binarySearch()
-    items = self.all
-    # Array containing one item per every page, its content being the last root on that page.
-    items = items.map {|p| p.last_root}
-    # Like binarySearch()
-    start_index = 0
-    stop_index  = (items.count-1)
-    middle = ((stop_index+start_index)/2).floor
-    retval = 0
-    # Instead of the loop_count-method for safeguarding against missing items.
-    (start_index..stop_index).each {|i| items[i] ||= ''}
-    # Should be safe to look up any value inside the items array now.
-    # It seems to always take 10 iterations anyways. And I don't like
-    # that it always comes up with /some/ results - that way, you can't
-    # tell the user that you couldn't find anything.
-    while (items[middle] != value && start_index < stop_index) do
-      logger.debug "*** most_likely_closeby_root: items[#{middle}]=#{items[middle].inspect}"
-      stop_index  = middle-1 if value < items[middle]
-      start_index = middle+1 if value > items[middle]
-      break if (middle != 0 && value < items[middle] && value > items[middle-1])
-      middle = ((stop_index + start_index)/2).floor
+  # Skip through all pages where the last root printed
+  # on them is alphabetically lower or equal to the root
+  # being searched for and then return the page after that.
+  def self.most_likely_page(search_value)
+    pages = self.all
+    first_possible_page = pages.first
+    i = 0
+    while pages[i].last_root <= search_value
+      i += 1
+      next if (pages[i+1] && (pages[i].last_root == pages[i+1].last_root))
     end
-    logger.debug "*** most_likely_closeby_root: items[#{middle}]=#{items[middle].inspect} (corrected)"
-    retval = 0 if middle == 0
-    middle = items.length-1 if middle > items.length-1
-    # There used to be a bit of code here which handled the
-    # case of the searched-for value directly being present
-    # in the searched list. I've removed it since that
-    # case is already being taken care of in find_by_root
-    # by querying the database. As far as could be seen,
-    # that code was never being exercised during searches.
-    if value > items[middle] && value.length == 1
-      retval = middle+1
-    else
-      retval = middle
-    end
-    # No idea what is going on above, but it's pretty darn
-    # cool and seems to yield very desirable results :-)
-    logger.debug "*** most_likely_closeby_root: items[#{retval}]=#{items[retval].inspect} (while searching for #{value.inspect})"
-    return items[retval].blank? ? nil : items[retval]
+    pages[i]
   end
 end
---
+++ test/models/page_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+ 
+class PageTest < ActiveSupport::TestCase
+  ROOT_PAGE_PAIRS = [
+    ['توح', 147],
+    ['ذرع', 427],
+    ['نصح', 1278],
+    ['نزف', 1262],
+    ['جرا', 173],
+    ['نزح', 1260],
+    ['شوه', 686],
+    ['انق', 49],
+    ['ادي', 15],
+    ['نظر', 1285],
+    ['ذهن', 434],
+    ['حيث', 313],
+    ['حير', 314],
+    ['وضح', 1409],
+    ['نصر', 1279],
+    ['قضو', 1035],
+    ['نمط', 1317],
+    ['ذهل', 434],
+    ['طوي', 795],
+    ['حول', 306],
+    ['جري', 179],
+    ['كب', 1080],
+    ['نشد', 1274],
+    ['ذقن', 428],
+    ['ثاب', 149],
+    ['نب', 1240],
+    ['نشي', 1276],
+    ['ملا', 1218],
+    ['لحي', 1148]
+  ]
+
+  test "all root searches return the correct page" do
+    ROOT_PAGE_PAIRS.each do |search_string, expected_page_number|
+      assert_equal(
+        Book.first.pages.find_by_root(search_string),
+        Book.first.pages.find_by_number(expected_page_number),
+        "search_string='#{search_string}'")
+    end
+  end
+end
