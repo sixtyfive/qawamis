aff9ca139b55444c5144179217e4f70c9f01dda3
J. R. Schmid <jrs+git@weitnahbei.de>
2016-05-12 20:30:15 +0200

    Adding template for adding new books.

---
+++ indices/mybook.js
@@ -0,0 +1,9 @@
+/* ------------------------------------------------- *
+ * Index of last article on every page of book <...> *
+ * Compiled by <...> (<...>).                        *
+ * ------------------------------------------------- */
+
+var <book name abbreviation> = [];
+var offset = <first content page as counted from book cover>;
+for (i=0; i<=offset; i++) <book name abbreviation>[i] = '';
+/* Real page number in comments */
