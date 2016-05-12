0d2db0289f212b543310ae2ef7fd237f3a3cd2f4
J. R. Schmid <jrs+git@weitnahbei.de>
2016-05-12 20:29:19 +0200

    Normalising HW5 file names and contents.

--- indices/hw5.sh
+++ indices/hw5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cat hw5.tpl.txt > mr-aa-hw5-index.js
+cat hw5.js.tpl > hw5.js
 i=26
 while read line; do
-  echo -e "/* $((i - 25)) */ hw5[$i]='$line';" >> mr-aa-hw5-index.js
+  echo -e "/* $((i - 25)) */ hw5[$i]='$line';" >> hw5.js
   i=$((i + 1))
 done < hw5.txt
